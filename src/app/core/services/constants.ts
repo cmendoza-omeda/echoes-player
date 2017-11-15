0bcb28fe41537f14f31bb4167e1209c355a86377
Chris Mendoza <cmendoza@omeda.com>
2017-11-15 17:11:54 -0500

    added whitelisted api key

--- src/app/core/services/constants.ts
+++ src/app/core/services/constants.ts
@@ -1,4 +1,4 @@
-export const YOUTUBE_API_KEY: string = 'AIzaSyDCGg6FG6s_zxACqel09vQUKBc-x26pKFA';
+export const YOUTUBE_API_KEY: string = 'AIzaSyBGlUdM8zI4LIGN6quP8CPOCr1zWzYXCFo';
 export const CLIENT_ID: string = '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs';
 export const X_CLIENT_DATA: string = ' CKe1yQEIiLbJAQiitskBCMS2yQEI+pzKAQipncoBCNyeygEIqKPKAQ==';
 export const REFERER: string = 'http://echoesplayer.com/';
